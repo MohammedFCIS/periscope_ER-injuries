5f5a34bba77a654a9f7f50e1ab9168704e4500b9
Mohammed Ali <moh_fcis@yahoo.com>
2021-12-26 14:34:06 +0200

    added basic ui layout

--- ER_Injuries/program/data/app_data.R
+++ ER_Injuries/program/data/app_data.R
@@ -1,3 +1,1 @@
-library(neiss)
 prod_codes <- setNames(products$code, products$title)
-
--- ER_Injuries/program/ui_body.R
+++ ER_Injuries/program/ui_body.R
@@ -22,7 +22,37 @@
 # ----------------------------------------
 
 # -- Create Elements
+diag <- box(
+  id          = "diag_box",
+  title       = "diagnosis",
+  width       = 4,
+  collapsible = TRUE,
+  collapsed   = FALSE,
+  fluidRow(tableOutput("diag_table")))
 
+body_part <- box(
+  id          = "body_part_box",
+  title       = "Body Part",
+  width       = 4,
+  collapsible = TRUE,
+  collapsed   = FALSE,
+  fluidRow(tableOutput("body_part_table")))
+
+location <- box(
+  id          = "location_box",
+  title       = "Location",
+  width       = 4,
+  collapsible = TRUE,
+  collapsed   = FALSE,
+  fluidRow(tableOutput("location_table")))
+
+age_sex <- box(
+  id          = "age_sex_box",
+  title       = "Age/Sex",
+  width       = 12,
+  collapsible = TRUE,
+  collapsed   = FALSE,
+  fluidRow(tableOutput("age_sex_table")))
 
 # -- Register Elements in the ORDER SHOWN in the UI
-add_ui_body()
+add_ui_body(list(diag, body_part, location, age_sex))
